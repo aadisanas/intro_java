2e4988bb85279e4ad10f0f346046e33c0efb355f
aadisanas <101534569+aadisanas@users.noreply.github.com>
2022-03-21 01:57:30 +0000

    explicit type casting.

--- Main.java
+++ Main.java
@@ -1,14 +1,12 @@
 class Main {
   public static void main(String[] args) {
-    int variableInt,variableA = 10 ;
-    variableInt = 1;
-    System.out.println(variableInt +"---"+ variableA);
-    float variableFloat;
-    variableFloat = 1.1f;
-    System.out.println(variableFloat);
-    boolean variableBoolean = true;
-    System.out.println(variableBoolean);
-      
+  float varFloat = 1.23f;
+  int varInt = (int)varFloat;
+  System.out.println(varInt);
+    
+    
+
+  
   
     
   
