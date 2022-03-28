e627dc829925218241b2ddfcf096531371504fe7
aadisanas <101534569+aadisanas@users.noreply.github.com>
2022-03-28 01:31:32 +0000

    finding if numbers are even or odd.

--- Main.java
+++ Main.java
@@ -1,13 +1,15 @@
 class Main {
   public static void main(String[] args) {
-    int operator1 = 10;
-    int operator2 = 15;
-    int sum, division, multiplication; 
-    sum = operator1 + operator2;
-    division = operator1 / operator2;
-    if(sum != division){
-      System.out.println("False");
+    int num = 11;
+    if(num%2 == 0){
+      System.out.println("This number is even!");
+    } else{
+      System.out.println("This number is odd!");
     }
+    
+  
+    
+    
     
     
     
