fbc399fec041e7e1b1ce7418bcd4d87b321f4337
aadisanas <101534569+aadisanas@users.noreply.github.com>
2022-03-28 01:26:48 +0000

    arithmetic operations

--- Main.java
+++ Main.java
@@ -1,8 +1,19 @@
 class Main {
   public static void main(String[] args) {
-  float varFloat = 1.23f;
-  int varInt = (int)varFloat;
-  System.out.println(varInt);
+    int operator1 = 10;
+    int operator2 = 15;
+    int sum, division, multiplication; 
+    sum = operator1 + operator2;
+    division = operator1 / operator2;
+    if(sum != division){
+      System.out.println("False");
+    }
+    
+    
+    
+    
+    
+    
     
     
 
