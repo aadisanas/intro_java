8b27c3d07a55373a170be353d032922205d7f582
aadisanas <101534569+aadisanas@users.noreply.github.com>
2022-04-18 01:41:36 +0000

    Finding out higher lowest and middle numbers.

--- Main.java
+++ Main.java
@@ -1,16 +1,50 @@
 class Main {
   public static void main(String[] args) {
-    int variableCompare = -10;
-    if(variableCompare > 0){
-      System.out.println("This number is greater than Zero !");
-    }else if(variableCompare == 0){
-      System.out.println("This number is equal to Zero !");
+    int variableOne = 10, variableTwo = 20, variableThree = 5;
+    
+  if(variableOne > variableTwo){
+    if(variableOne > variableThree){
+      System.out.println("Highest = "+ variableOne);
     }else{
-      System.out.println("This number is less than Zero !");
+      System.out.println("Highest = "+ variableThree);
     }
+  }else{
+    if(variableTwo > variableThree){
+      System.out.println("Highest =" + variableTwo);
+    }else{
+      System.out.println("Highest = "+ variableThree);
+    }
+  } 
+
+
+  if(variableOne < variableTwo){
+    if(variableOne < variableThree){
+      System.out.println("Lowest = "+ variableOne);
+    }else{
+      System.out.println("Lowest = "+ variableThree);
+    }
+  }else{
+    if(variableTwo < variableThree){
+      System.out.println("Lowest =" + variableTwo);
+    }else{
+      System.out.println("Lowest = "+ variableThree);
+    }
+  } 
+
+  if(variableOne > variableTwo){
+    if(variableOne < variableThree){
+      System.out.println("Middle = "+ variableOne);
+    }else{
+      if(variableThree > variableTwo){
+        System.out.println("Middle = "+ variableThree);
+      }else{
+        System.out.println("Middle =" + variableTwo);
+      }
+    }
+  }else
+    
       
-    
-    
+  }
       
       
     
