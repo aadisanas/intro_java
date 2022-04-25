866f465071963cf9ac832d788ea5d7b833f8f768
aadisanas <101534569+aadisanas@users.noreply.github.com>
2022-04-25 01:56:32 +0000

    How to use while loops.

--- Main.java
+++ Main.java
@@ -1,38 +1,18 @@
 class Main {
   public static void main(String[] args) {
-    int varOne = 50,varTwo = 25,varThree = 9;
-    int smallest=0,highest=0,secondHighest=0;
-    if((varOne > varTwo) && (varOne > varThree)){
-      highest = varOne;
-      if(varTwo > varThree){
-        secondHighest = varTwo;
-        smallest = varThree;
-      }else{
-        secondHighest = varThree;
-        smallest = varTwo;
-      }
-    }else if((varTwo > varOne) && (varTwo > varThree)){
-      highest = varTwo;
-      if(varOne > varThree){
-        secondHighest = varOne;
-        smallest = varThree;
-      }else{
-        secondHighest = varThree;
-        smallest = varOne;
-      }
-    }else{
-      highest = varThree;
-      if(varOne > varTwo){
-        secondHighest = varOne;
-        smallest = varTwo;
-      }else{
-        secondHighest = varTwo;
-        smallest = varOne;
-      }
+    int number = 12345;
+    while(number > 0){
+      System.out.println(number%10);
+      number = number/10;
+      
     }
     
-    System.out.println("Highest = "+highest);
-    System.out.println("secondHighest = "+secondHighest);
-    System.out.println("smallest = "+smallest);
-  }
-}
+    
+        
+      
+    }
+        
+      
+      }
+    
+
