c6f9c18797abc52deaa856a6c03005d42632d5c9
aadisanas <101534569+aadisanas@users.noreply.github.com>
2022-10-11 01:51:56 +0000

    using strings

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 public class Main{
   public static void main(String args[]){
-    Dell c= new Dell();
-    c.camera();
-    c.keyboard();
+    Scanner s= new Scanner(System.in);
+    String firstname = s.next();
+    String lastname = s.next();
+    String emailid = firstname + "." + lastname + "123" + "@gmail.com";
+    String emailid2 = firstname.substring(0,3) + "." + lastname.substring(lastname.length()-3) + "123@gmail.com" ;
+    System.out.println(emailid);
+    System.out.print(emailid2);
+    
   }
 }
